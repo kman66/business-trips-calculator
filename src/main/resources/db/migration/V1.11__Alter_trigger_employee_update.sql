9859a8ee783f0b1c62a6e0a35bcffb1da088d3fc
kman66 <karol.manterys@gmail.com>
2018-10-19 19:06:20 +0200

    creating audit tables and triggers for currency and parameter

--- src/main/resources/db/migration/V1.11__Alter_trigger_employee_update.sql
+++
@@ -1,7 +0,0 @@
-DROP TRIGGER employee_update ON EMPLOYEES;
-
-CREATE TRIGGER employee_update
-  AFTER UPDATE
-  ON EMPLOYEES
-  FOR EACH ROW
-EXECUTE PROCEDURE log_employee_after_update();
--- src/main/resources/db/migration/V1.4__Create_trigger_employee_insert.sql
+++
@@ -1,5 +0,0 @@
-CREATE TRIGGER employee_insert
-  AFTER INSERT
-  ON EMPLOYEES
-  FOR EACH ROW
-  EXECUTE PROCEDURE log_employee_after_insert();
